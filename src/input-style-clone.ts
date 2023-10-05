172783dd7a25ba033e9b399b6e197dc4dd4e3cae
Ian Sanders <iansan5653@github.com>
2023-10-05 13:41:10 +0000

    Add attribution notes

--- src/input-style-clone.ts
+++ src/input-style-clone.ts
@@ -1,6 +1,13 @@
 export type InputElement = HTMLTextAreaElement;
 
-/** Create a `div` that exactly matches an input element in all but position. */
+/**
+ * Create a `div` that exactly matches an input element in all but position.
+ * 
+ * PRIOR ART: This approach & code was adapted from the following MIT-licensed sources:
+ * - primer/react (Copyright (c) 2018 GitHub, Inc.): https://github.com/primer/react/blob/a0db832302702b869aa22b0c4049ad9305ef631f/src/drafts/utils/character-coordinates.ts
+ * - koddsson/textarea-caret-position (Copyright (c) 2015 Jonathan Ong me@jongleberry.com): https://github.com/koddsson/textarea-caret-position/blob/eba40ec8488eed4d77815f109af22e1d9c0751d3/index.js
+ * - component/textarea-caret-position (Copyright (c) 2015 Jonathan Ong me@jongleberry.com): https://github.com/component/textarea-caret-position/blob/b5db7a7e47dd149c2a66276183c69234e4dabe30/index.js
+ */
 export class InputStyleClone {
   #mutationObserver = new MutationObserver(() => this.#updateStyles());
   #resizeObserver = new ResizeObserver(() => this.#updateStyles());
