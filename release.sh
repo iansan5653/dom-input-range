892c9023e9f1e87dbdbc035bd414ffa070767147
Ian Sanders <iansan5653@github.com>
2023-10-09 14:48:31 +0000

    Add validation step to release script

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
-# usage: `release.sh [<version> | patch | minor | major]`
 set -e
+
+if [ -z "$1" ]; then
+  echo "usage: release.sh [<version> | patch | minor | major]"
+  exit 1
+fi
 
 TAG=$(npm version $1 --git-tag-version)
 git push
