3ced1c4b044d43a6f54da065d8a2f703d81dddbb
Ian Sanders <iansan5653@github.com>
2024-02-23 17:03:30 +0000

    Create a `CustomHtmlElement` abstract class and improve docs more

--- src/input-style-clone-element.ts
+++ src/input-style-clone-element.ts
@@ -1,3 +1,5 @@
+import { CustomHTMLElement } from "./custom-html-element.js";
+
 export type InputElement = HTMLTextAreaElement | HTMLInputElement;
 
 export class InputStyleCloneUpdateEvent extends Event {
@@ -18,14 +20,15 @@ const CloneRegistry = new WeakMap<InputElement, InputStyleCloneElement>();
  * Emits `update` events whenever anything is recalculated: when the layout changes, when the user scrolls, when the
  * input is updated, etc. This event may be emitted more than once per change.
  *
- * NOTE! This class will not auto update if `input.value` is set directly!
- *
+ * @note There may be cases in which the clone cannot observe changes to the input and fails to automatically update.
+ * For example, if the `value` property on the input is written to directly, no `input` event is emitted by the input
+ * and the clone does not automatically update. In these cases, `forceUpdate` can be used to manually trigger an update.
  */
 // PRIOR ART: This approach was adapted from the following MIT-licensed sources:
 //  - primer/react (Copyright (c) 2018 GitHub, Inc.): https://github.com/primer/react/blob/a0db832302702b869aa22b0c4049ad9305ef631f/src/drafts/utils/character-coordinates.ts
 //  - component/textarea-caret-position (Copyright (c) 2015 Jonathan Ong me@jongleberry.com): https://github.com/component/textarea-caret-position/blob/b5db7a7e47dd149c2a66276183c69234e4dabe30/index.js
 //  - koddsson/textarea-caret-position (Copyright (c) 2015 Jonathan Ong me@jongleberry.com): https://github.com/koddsson/textarea-caret-position/blob/eba40ec8488eed4d77815f109af22e1d9c0751d3/index.js
-export class InputStyleCloneElement extends HTMLElement {
+export class InputStyleCloneElement extends CustomHTMLElement {
   #styleObserver = new MutationObserver(() => this.#updateStyles());
   #resizeObserver = new ResizeObserver(() => this.#requestUpdateLayout());
 
@@ -39,7 +42,11 @@ export class InputStyleCloneElement extends HTMLElement {
   /**
    * Get the clone for an input, reusing an existing one if available. This avoids creating unecessary clones, which
    * have a performance cost due to their high-frequency event-based updates. Because these elements are shared, they
-   * should NOT be mutated directly.
+   * should be mutated with caution.
+   *
+   * Upon initial creation the clone element will automatically be inserted into the DOM and begin observing the
+   * linked input. Only one clone per input can ever exist at a time.
+   * @param input The target input to clone.
    */
   static for(input: InputElement) {
     const clone = CloneRegistry.get(input) ?? new InputStyleCloneElement(input);
@@ -47,7 +54,10 @@ export class InputStyleCloneElement extends HTMLElement {
     return clone;
   }
 
-  /** Avoid constructing directly: Use `InputStyleCloneElement.for` instead. */
+  /**
+   * Avoid constructing directly: Use `InputStyleCloneElement.for` instead.
+   * @private
+   */
   constructor(input: InputElement) {
     super();
 
@@ -72,7 +82,7 @@ export class InputStyleCloneElement extends HTMLElement {
 
   /** @private */
   connectedCallback() {
-    const input = this.#inputElement;
+    const input = this.#inputRef.deref();
     if (!input) return this.remove();
 
     this.style.pointerEvents = "none";
@@ -116,7 +126,7 @@ export class InputStyleCloneElement extends HTMLElement {
     document.removeEventListener("scroll", this.#onDocumentScrollOrResize, { capture: true });
     window.removeEventListener("resize", this.#onDocumentScrollOrResize, { capture: true });
 
-    const input = this.#inputElement;
+    const input = this.#inputRef.deref();
     if (input) {
       input.removeEventListener("input", this.#onInput);
       CloneRegistry.delete(input);
@@ -125,21 +135,24 @@ export class InputStyleCloneElement extends HTMLElement {
 
   // --- private ---
 
-  get #inputElement() {
-    return this.#inputRef.deref();
-  }
-
+  /** Perform `fn` using the `input` if it is still available. If not, clean up the clone instead. */
   #usingInput<T>(fn: (input: InputElement) => T | void) {
-    const input = this.#inputElement;
+    const input = this.#inputRef.deref();
     if (!input) return this.remove();
     return fn(input);
   }
 
+  /** Current relative x-adjustment in pixels, executed via CSS transform. */
   #xOffset = 0;
+  /** Current relative y-adjustment in pixels, executed via CSS transform. */
   #yOffset = 0;
 
-  /** Update only geometric properties without recalculating styles. */
+  /**
+   * Update only geometric properties without recalculating styles. Typically call `#requestUpdateLayout` instead to
+   * only update once per animation frame.
+   */
   #updateLayout() {
+    // This runs often, so keep it as fast as possible! Avoid all unecessary updates.
     this.#usingInput((input) => {
       const inputStyle = window.getComputedStyle(input);
 
@@ -147,8 +160,10 @@ export class InputStyleCloneElement extends HTMLElement {
       this.style.width = inputStyle.width;
 
       // Immediately re-adjust for browser inconsistencies in scrollbar handling, if necessary
-      this.style.height = `calc(${inputStyle.height} + ${input.clientHeight - this.clientHeight}px)`;
-      this.style.width = `calc(${inputStyle.width} + ${input.clientWidth - this.clientWidth}px)`;
+      if (input.clientHeight !== this.clientHeight)
+        this.style.height = `calc(${inputStyle.height} + ${input.clientHeight - this.clientHeight}px)`;
+      if (input.clientWidth !== this.clientWidth)
+        this.style.width = `calc(${inputStyle.width} + ${input.clientWidth - this.clientWidth}px)`;
 
       // Position on top of the input
       const inputRect = input.getBoundingClientRect();
@@ -168,6 +183,7 @@ export class InputStyleCloneElement extends HTMLElement {
 
   #isLayoutUpdating = false;
 
+  /** Request a layout update. Will only happen once per animation frame, to avoid unecessary updates. */
   #requestUpdateLayout() {
     if (this.#isLayoutUpdating) return;
     this.#isLayoutUpdating = true;
@@ -178,6 +194,7 @@ export class InputStyleCloneElement extends HTMLElement {
     });
   }
 
+  /** Update the styles of the clone based on the styles of the input, then request a layout update. */
   #updateStyles() {
     this.#usingInput((input) => {
       const inputStyle = window.getComputedStyle(input);
@@ -188,12 +205,17 @@ export class InputStyleCloneElement extends HTMLElement {
     });
   }
 
+  /**
+   * Update the text content of the clone based on the text content of the input. Triggers a layout update in case the
+   * text update caused scrolling.
+   */
   #updateText() {
     this.#usingInput((input) => {
       this.textContent = input.value;
 
-      // No need to update layout; if the text causes a scroll it will trigger a layout change via event listener
-      this.dispatchEvent(new InputStyleCloneUpdateEvent());
+      // This is often unecessary on a pure text update, but text updates could potentially cause layout updates like
+      // scrolling or resizing. And we run the update on _every frame_ when scrolling, so this isn't that expensive.
+      this.#requestUpdateLayout();
     });
   }
 
